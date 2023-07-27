926c5fdc697d23de0e7803a10e989ef7258f38aa
Akhil Dhanesh <akhildhanesh1@outlook.com>
2023-07-27 12:12:37 +0530

    ultrasonic sensor

--- run.js
+++ run.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { Gpio } from 'onoff'
 import * as dotenv from 'dotenv'
+import rpio from 'rpio'
 
 dotenv.config() // for using .env files
 
@@ -9,97 +10,89 @@ const TITLE = 'AFC' // Pushover Notification Title
 
 const UID = process.env.UID // Unique product ID
 
-const IrPin = new Gpio(4, 'in', 'both')
-const PirPin = new Gpio(27, 'in', 'both')
+const BuzzerPin = new Gpio(27, 'out')
 const relayPin = new Gpio(6, 'out')
 
-let IR = false // Will be true if IR Sensor detect something in front of it
-let PIR = false // Will be true if IR sensor detect something and No One is there in the kitchen
-let timer1 = {}
-let timer2 = {}
-let timer3 = {}
-let relayState = false // Relay is turned ON or OFF
+const triggerPin = 17
+const echoPin = 18
 
-IrPin.watch((err, value) => {
-    if (err) throw err
+rpio.init({ gpiomem: false })
+rpio.open(triggerPin, rpio.OUTPUT, rpio.LOW)
+rpio.open(echoPin, rpio.INPUT)
 
-    if (value) {
-        IR = true //object infront of IR
-        console.log('IR: Object detected!')
-    } else {
-        IR = false //No object 
-        if (relayState) {
-            relayPin.writeSync(0) //No Object in front of IR -> So, relay OFF
-        }
-        relayState = false // relay is OFF
-        console.log('IR: No object detected.')
-    }
-})
 
-PirPin.watch((err, value) => {
-    if (err) throw err
+const measureDistance = () => {
+    rpio.write(triggerPin, rpio.HIGH)
+    rpio.usleep(10)
+    rpio.write(triggerPin, rpio.LOW)
 
-    if (value) { //If movement detected
-        if (PIR) { // if timers was created -> clear all the timer (since someone is there)
-            clearTimeout(timer1)
-            clearTimeout(timer2)
-            clearTimeout(timer3)
-            PIR = false // PIR last state was changed back to false -> No movement Detected
-        }
-        console.log('PIR: Movement Detected');
-    } else { //If No One is there (No Movement Detected)
-        if (IR) { // and something is there in front of the IR sensor
-            PIR = true // then change the state of PIR to true indicating No One is there and IR is active
+    while (rpio.read(echoPin) === 0) { }
+    const start = process.hrtime.bigint()
+
+    while (rpio.read(echoPin) === 1) { }
+    const end = process.hrtime.bigint()
+
+    const pulseDuration = end - start
+    const distance = Number(pulseDuration) / 29.412 / 2 // in cm
+
+    return distance
+}
+
+const personDetected = () => measureDistance() < 100
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+async function main() {
+    console.log('Monitoring Sensors')
+    let timer1, timer2, timer3
+    let timerState = false
+    while (true) {
+        await sleep(1000)
+        if (personDetected()) {
+            if (timerState) {
+                clearTimeout(timer1)
+                clearTimeout(timer2)
+                clearTimeout(timer3)
+                timerState = false
+                console.log('Timers: Stopped')
+            }
+        } else {
+            if (timerState) continue
+            timerState = true
+            console.log('Timers: Started')
             timer1 = setTimeout(() => {
-                const data = {
-                    key: UID,
-                    title: TITLE,
-                    message: `Warning: 1`
-                }
-                axios.post(URL, data) // send http post request to the Server
-                    .then(res => res.data)
-                    .then(data => console.log(data))
-                    .catch(e => console.error(e.message))
-            }, 60000)
+                BuzzerPin.writeSync(1)
+                console.log('Buzzer: ON')
+            }, 30000)
             timer2 = setTimeout(() => {
                 const data = {
                     key: UID,
                     title: TITLE,
-                    message: `Warning: 2`,
+                    message: `Warning => ......`,
                     priority: 1
                 }
-                axios.post(URL, data) // send http post request to the Server
+                axios.post(URL, data) 
                     .then(res => res.data)
                     .then(data => console.log(data))
                     .catch(e => console.error(e.message))
-            }, 300000)
+            }, 60000)
             timer3 = setTimeout(() => {
                 relayPin.writeSync(1)
                 relayState = true
                 const data = {
                     key: UID,
                     title: TITLE,
-                    message: `Warning: 3 => Turning Off`,
+                    message: `OFF`,
                     priority: 1,
-                    // priority: 2,
-                    // expire: 3600,
-                    // retry: 20
                 }
-                axios.post(URL, data) // send http post request to the Server
+                axios.post(URL, data) 
                     .then(res => res.data)
                     .then(data => console.log(data))
                     .catch(e => console.error(e.message))
-            }, 600000)
-            console.log('PIR: Triggered -> No One is there in the kitchen and something is there in front of IR Sensor')
+            }, 90000)
         }
     }
-})
+}
 
-process.on('SIGINT', () => {
-    IrPin.unexport()
-    PirPin.unexport()
-    relayPin.unexport()
-    process.exit()
-});
-
-console.log('Monitoring Sensors')
+main()
+.catch(err => console.error(`Error: ${err}`))
